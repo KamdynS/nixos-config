ebf36eab722a27fb088c8430dd28f788ae81b25c
Kamdyn Shaeffer <kamdynshaefferbusiness@gmail.com>
2026-02-02 19:38:43 -0500

    keybinds

--- home/niri.nix
+++ home/niri.nix
@@ -153,7 +153,7 @@
       "Ctrl+Alt+Print".action.spawn = [ "bash" "-c" "grim - | wl-copy" ];
 
       # Keybinding help (Mod+F1)
-      "Mod+;".action.spawn = [ qs "ipc" "call" "keybinds" "toggle" ];
+      "Mod+W".action.spawn = [ qs "ipc" "call" "keybinds" "toggle" ];
       
       # Exit
       "Mod+Shift+E".action.quit = [];
